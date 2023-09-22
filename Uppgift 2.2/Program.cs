543421469ef3913ca3fc4a464989441826a57629
Pingvu <nhlpool27@gmail.com>
2023-09-22 14:43:36 +0200

    Add project files.

---
+++ Uppgift 2.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace uppgift
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv hur långt Alma hoppade i meter.");
+            string Alma = Console.ReadLine();
+            double AlmaHopp = double.Parse(Alma);
+
+
+
+            Console.Clear();
+            Console.WriteLine("Skriv hur långt Elin hoppade i meter. (längre än Alma)");
+            string Elin = Console.ReadLine();
+            double ElinHopp = double.Parse(Elin);
+
+            double skillnad = ElinHopp - AlmaHopp;
+
+            Console.Clear();
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma." ); // --=* GJORD AV NOAH *=--
+
+
+        }
+
+    }
+
+}
